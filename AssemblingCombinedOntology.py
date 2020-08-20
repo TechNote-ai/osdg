5cbe473449d8acf68ba87deac55f3861349a0e2b
SimonasTr <trepocka.simonas@gmail.com>
2020-08-20 14:46:25 +0300

    renaming_variables_for_readability

--- AssemblingCombinedOntology.py
+++ AssemblingCombinedOntology.py
@@ -13,40 +13,38 @@ import os
 
 directories = [i for i in os.listdir("./Raw_Data/") if "." not in i]
 
-sdg_ontology_combined = {"SDG_1" : {} , 
-                        "SDG_2" : {} , 
-                        "SDG_3" : {} , 
-                        "SDG_4" : {} , 
-                        "SDG_5" : {} , 
-                        "SDG_6" : {} , 
-                        "SDG_7" : {} , 
-                        "SDG_8" : {} , 
-                        "SDG_9" : {} , 
-                        "SDG_10" : {} , 
-                        "SDG_11" : {} , 
-                        "SDG_12" : {} , 
-                        "SDG_13" : {} , 
-                        "SDG_14" : {} , 
-                        "SDG_15" : {} , 
-                        "SDG_16" : {} , 
-                        "SDG_17" : {} , }
+sdg_ontology_combined = {
+    "SDG_1" : {} , 
+    "SDG_2" : {} , 
+    "SDG_3" : {} , 
+    "SDG_4" : {} , 
+    "SDG_5" : {} , 
+    "SDG_6" : {} , 
+    "SDG_7" : {} , 
+    "SDG_8" : {} , 
+    "SDG_9" : {} , 
+    "SDG_10" : {} , 
+    "SDG_11" : {} , 
+    "SDG_12" : {} , 
+    "SDG_13" : {} , 
+    "SDG_14" : {} , 
+    "SDG_15" : {} , 
+    "SDG_16" : {} , 
+    "SDG_17" : {} , 
+    }
 
 
-for d in directories :
-    file_name = [i for i in os.listdir( "./Raw_Data/"+d+"/") if "_ProcessedKeyTerms.json" in i ][0]
-    file = open( "./Raw_Data/"+d+"/"+file_name , "r")
-    data = json.loads( file.read() )
-    file.close()
+for directory in directories :
+    file_name = [ i for i in os.listdir( "./Raw_Data/" + directory + "/" ) if "_ProcessedKeyTerms.json" in i ][0]
+    with open( f'./Raw_Data/{directory}/{file_name}', 'r' ) as file_:
+        processed_key_terms = json.loads( file_.read() )
     
-    for key , value in data.items() :
-        for v in value :
-            if v not in sdg_ontology_combined[ key ].keys() :
-                sdg_ontology_combined[ key ][ v ] =[ d ]
-            else:
-                sdg_ontology_combined[ key ][ v ].append( d )
+    for sdg_label , key_terms in processed_key_terms.items() :
+        for term in key_terms :
+            if term not in sdg_ontology_combined[ sdg_label ].keys() :
+                sdg_ontology_combined[ sdg_label ][ term ] = []
+            sdg_ontology_combined[ sdg_label ][ term ].append( directory )
                 
 #%%
-js = json.dumps( sdg_ontology_combined )
-file = open("CombinedOntology.json" , "w")
-file.write( js )
-file.close()
+with open("CombinedOntology.json" , "w") as file_:
+    file_.write( json.dumps( sdg_ontology_combined ) )
