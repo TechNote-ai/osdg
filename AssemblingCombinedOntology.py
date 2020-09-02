68349a06f9f5804fcd0869e61aba56a32dab2d04
SimonasTr <trepocka.simonas@gmail.com>
2020-09-02 12:38:37 +0300

    assembl comb ont checked

--- AssemblingCombinedOntology.py
+++ AssemblingCombinedOntology.py
@@ -53,7 +53,7 @@ for directory in add_validated_data_paths:
         for fos in sdg_fos_add_validated[sdg_label]:
             if fos not in fos_sources[sdg_label].keys():
                 fos_sources[sdg_label][fos] = []
-            fos_sources[sdg_label][fos].append(directory)
+            fos_sources[sdg_label][fos].append(directory.split('/')[-1])
 
 # All to all    # TODO leave it for matching? if not, it goes into assembling sdg_fos_script. Must be checked for conflicts when assembling generated
 for directory in add_all_to_all_data_paths:
@@ -77,7 +77,7 @@ for directory in add_all_to_all_data_paths:
         for fos in sdg_fos_add_validated[sdg_label]:
             if fos not in fos_sources[sdg_label].keys():
                 fos_sources[sdg_label][fos] = []
-            fos_sources[sdg_label][fos].append(directory)
+            fos_sources[sdg_label][fos].append(directory.split('/')[-1])
 
 sdg_fos_add_validated = {
     sdg_label: sorted(list(sdg_fos_add_validated[sdg_label])) 
@@ -159,7 +159,7 @@ for sdg_label in sdg_labels:
     for fos in sorted(list(set(validated_fos + generated_fos))):
         if fos not in sdg_ontology_combined[sdg_label].keys():
             sdg_ontology_combined[sdg_label][fos] = dict()
-        sdg_ontology_combined[sdg_label][fos] = fos_sources[sdg_label][fos].split('/')[-1]
+        sdg_ontology_combined[sdg_label][fos] = fos_sources[sdg_label][fos]
 
 with open("CombinedOntology.json" , "w") as file_:
     file_.write(json.dumps(sdg_ontology_combined))
